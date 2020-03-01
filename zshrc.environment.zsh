c9ca3f45513889ca8ab07a9a5777688b133161de
Joe Colosimo <jwc@jwcxz.com>
2020-03-01 14:53:31 -0800

    set LANG if not set, set EDITOR(/VIEWER) to nvim if it's installed

--- zshrc.environment.zsh
+++ zshrc.environment.zsh
@@ -2,10 +2,19 @@ HISTFILE=~/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
 
-export EDITOR=vim
-export VIEWER=vim
+if [[ "$LANG" == "" ]]; then
+    export LANG="en_US.UTF-8"
+fi
+
+if command -v nvim >/dev/null 3>&1; then
+    export EDITOR=nvim
+else
+    export EDITOR=vim
+fi
+
+export VIEWER="$EDITOR"
+
 export KEYTIMEOUT=1
-export NVIM_TUI_ENABLE_CURSOR_SHAPE=1
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
 alias grep="grep --color=auto"
