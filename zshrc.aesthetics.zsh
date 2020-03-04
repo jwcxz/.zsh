ba8c583c82710a8cb6f4120bad2f759a1412f3db
Joe Colosimo <jwc@jwcxz.com>
2020-03-03 20:31:03 -0800

    add date/time to prompt and change color of current directory to cyan

--- zshrc.aesthetics.zsh
+++ zshrc.aesthetics.zsh
@@ -8,19 +8,24 @@ local tl="%F{246}┌ %{$reset_color%}"
 local bl="%F{246}└ %{$reset_color%}"
 
 
-function git_prompt_info() {
+function prompt_git_branch() {
     ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
     ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
     echo "${sep}%{$fg[yellow]%}⌥ ${ref#refs/heads/}%{$reset_color%}"
 }
 
+function prompt_date_time() {
+    echo "${sep}%{$fg[magenta]%}%D{%Y-%m-%d} %{$terminfo[bold]$fg[magenta]%}%D{%H:%M:%S}%{$reset_color%}"
+}
+
 
 local user_host='%{$terminfo[bold]$fg[green]%}%n@%m%{$reset_color%}'
-local current_dir='%{$terminfo[bold]$fg[blue]%}%~%{$reset_color%}'
-local git_branch='$(git_prompt_info)%{$reset_color%}'
+local current_dir='%{$terminfo[bold]$fg[cyan]%}%~%{$reset_color%}'
+local current_git_branch='$(prompt_git_branch)%{$reset_color%}'
+local current_date_time='$(prompt_date_time)%{$reset_color%}'
 
 
-PROMPT="${tl}${user_host}${sep}${current_dir}${git_branch}
+PROMPT="${tl}${user_host}${sep}${current_dir}${current_date_time}${current_git_branch}
 ${bl}%B$%b "
 
 setopt prompt_subst
